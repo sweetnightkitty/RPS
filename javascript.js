89914f145c26f579832a79a013319ee25d793b9b
SaraToth <saratoth101@gmail.com>
2023-09-12 22:12:25 +0900

    create two solution options and complete RPS without a loop

--- javascript.js
+++ javascript.js
@@ -29,16 +29,73 @@ const computerSelection = getComputerChoice();
 //runs 5 rounds and logs how many wins out of total rounds.
 function game() {
     let playerSelection = prompt('Choose rock, scissors, or paper');
-    let round = playRound(playerSelection, computerSelection);
-    let win = 0;
-    let totalRounds = 1;
+    let round1 = playRound
     console.log(playRound(playerSelection, computerSelection));
-    if (round.includes('win')) {
-        win += 1;
-    }
-    return('(' + win + '/' + totalRounds + ')');
-    playerSelection = prompt('Choose again');
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
 }
 
-console.log(game());
 
+
+
+
+
+
+
+
+
+const playerSelection = 'rock';
+
+
+//ternary operator that determines if user win, lose, or draw
+function defineResult(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let result = player == 'rock' && computerSelection == 'Scissors' || player == 'scissors' && computerSelection == 'Paper' || player == 'paper' && computerSelection == 'Rock' ? 'win' 
+    : player == 'rock' && computerSelection == 'Paper' || player == 'paper' && computerSelection == 'Scissors' || player == 'scissors' && computerSelection == 'Rock' ? 'lose'
+    : 'draw';
+    return result;
+}
+const result = defineResult(playerSelection, computerSelection);
+
+//function defines what option won the game (Rock, Scissors or Paper) regardless of who won
+function defineWinningChoice() {
+    let winningChoice = result == 'win' ? playerSelection 
+    : result == 'lose' ? computerSelection
+    : '';
+    return winningChoice.toLowerCase();
+}
+
+const winningChoice = defineWinningChoice();
+
+//Creates a message for each winning option
+function getRule() {
+    return winningChoice == 'rock' ? 'Rock beats scissors!'
+    : winningChoice == 'paper' ? 'Paper beats rock!'
+    : winningChoice == 'scissors' ? 'Scissors beats paper!'
+    : winningChoice == ''? 'Try again.'
+    : 'error';
+}
+
+const rule = getRule();
+
+//Function that gets a message after the game
+function showExpression() {
+    return 'You ' + result + '! ' + rule;
+
+}
+
+//5 round game
+function fiveRound() {
+    let playerSelection = prompt('Choose rock, scissors, or paper');
+    let round1 = console.log(showExpression());
+    let round2 = console.log(showExpression());
+    let round3 = console.log(showExpression());
+    let round4 = console.log(showExpression());
+    let round5 = console.log(showExpression());
+    
+
+}
+
+fiveRound();
