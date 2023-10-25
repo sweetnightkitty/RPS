0ee6a210fd0b7a1c932a5567d65fbe33bc36dc1d
SaraToth <saratoth101@gmail.com>
2023-10-25 15:25:27 +0900

    Add loop to 5 round RPS game, needs score log and final winner

--- javascript.js
+++ javascript.js
@@ -25,11 +25,16 @@ function validateInput(input) {
 function playRound() {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
+
     const winner = checkWinner(playerSelection, computerSelection);
     const winningChoice = getWinningChoice(winner, playerSelection, computerSelection);
+
     const rule = getRule(winningChoice);
     const message = getMessage(winner);
+    
+    //const score = getScore(); - need to create this function below
     console.log(message + rule);
+    //console.log(score);
     
 }
 
@@ -43,7 +48,7 @@ function getWinningChoice(winner, player, computer) {
     }
     }
 
-//outputs You win, you lose, or its a draw! - might need to add a space.
+//outputs You win, you lose, or its a draw!
 function getMessage(winner) {
     if (winner === 'player'){
         return 'You win! ';
@@ -80,14 +85,35 @@ function getRule(winningChoice) {
     }
 }
 
+/* function getScore() { //called from playRound, will create the updated score for player and computer; will log in playRound()
+    const playerScore =;
+    const computerScore;
+    const message = 'Player: ' + playerScore + ' ' + 'Computer: ' + computerScore;
+} */
 
 
-function game() {
-    const roundOne = playRound();
-    const roundTwo = playRound();
-    const roundThree = playRound();
-    const roundFour = playRound();
-    const roundFive = playRound();
+
+function game() { //score is not defined; create variable score to be Player:# Computer:#
+    let computerScore = 0;
+    let playerScore = 0;
+    for (i = 0; i < 6; i++) {
+        let round = playRound();
+    if (round.includes('win')) {
+        playerScore++
+        console.log(score);
+    } else if (round.includes('lose')){
+        computerScore++
+        console.log(score);
+    } else {
+        console.log(score);
+    }
+} //add code here to announce the winner when the for loop ends and rounds finish
 }
 
+
 game();
+
+/*Need a loop that will:
+- play 1 round; print the message from playRound() AND update the computer / player score
+- Stops after 5 rounds
+- then announces winner*/
